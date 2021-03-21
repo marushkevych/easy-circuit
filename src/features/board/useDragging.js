e9319e0cf3fda3b7a168e4c804c6273cce94c005
Marushkevych <marushkevych@gmail.com>
2021-03-21 02:16:30 -0400

    improve useDragging

--- src/features/board/useDragging.js
+++ src/features/board/useDragging.js
@@ -5,22 +5,6 @@ export default function useDragging() {
   const [pos, setPos] = useState({ x: 0, y: 0 });
   const ref = useRef(null);
 
-  function onMouseMove(e) {
-    if (!isDragging) return;
-    setPos({
-      x: e.x - ref.current.offsetWidth / 2,
-      y: e.y - ref.current.offsetHeight / 2,
-    });
-    e.stopPropagation();
-    e.preventDefault();
-  }
-
-  function onMouseUp(e) {
-    setIsDragging(false);
-    e.stopPropagation();
-    e.preventDefault();
-  }
-
   function onMouseDown(e) {
     if (e.button !== 0) return;
     setIsDragging(true);
@@ -36,26 +20,42 @@ export default function useDragging() {
 
   // When the element mounts, attach an mousedown listener
   useEffect(() => {
-    ref.current.addEventListener("mousedown", onMouseDown);
+    const currentRef = ref.current
+    currentRef.addEventListener("mousedown", onMouseDown);
 
     return () => {
-      ref.current.removeEventListener("mousedown", onMouseDown);
+      currentRef.removeEventListener("mousedown", onMouseDown);
     };
-  }, [ref.current]);
+  }, []);
 
   // Everytime the isDragging state changes, assign or remove
   // the corresponding mousemove and mouseup handlers
   useEffect(() => {
+    function onMouseMove(e) {
+      if (!isDragging) return;
+      setPos({
+        x: e.x - ref.current.offsetWidth / 2,
+        y: e.y - ref.current.offsetHeight / 2,
+      });
+      // e.stopPropagation();
+      // e.preventDefault();
+    }
+
+    function onMouseUp(e) {
+      setIsDragging(false);
+      // e.stopPropagation();
+      // e.preventDefault();
+    }
+
     if (isDragging) {
       document.addEventListener("mouseup", onMouseUp);
       document.addEventListener("mousemove", onMouseMove);
-    } else {
-      document.removeEventListener("mouseup", onMouseUp);
-      document.removeEventListener("mousemove", onMouseMove);
     }
     return () => {
-      document.removeEventListener("mouseup", onMouseUp);
-      document.removeEventListener("mousemove", onMouseMove);
+      if (isDragging) {
+        document.removeEventListener("mouseup", onMouseUp);
+        document.removeEventListener("mousemove", onMouseMove);
+      }
     };
   }, [isDragging]);
 
