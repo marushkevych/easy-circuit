250ee7d72560636f36130aa3098c4019c38e1131
Marushkevych <marushkevych@gmail.com>
2022-11-10 14:52:10 -0500

    Distinguish between click and drag

--- src/features/board/components/Switch.js
+++ src/features/board/components/Switch.js
@@ -1,16 +1,39 @@
 import React, { useState } from 'react'
 import SwitchOn from '../shapes/SwitchOn'
 import SwitchOff from '../shapes/SwitchOff';
+import styles from './components.module.css';
 
 export default function Switch() {
 
   const [isOn, setIsOn] = useState(false);
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [isMoving, setIsMoving] = useState(false);
 
-  const handleClick = () => setIsOn(!isOn);
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+    if (!isMoving) {
+      setIsOn(!isOn);
+    }
+
+    setIsMoving(false);
+  }
+
+  const handleMouseMove = () => {
+    if(isMouseDown) {
+      setIsMoving(true);
+    }
+  }
 
   return (
-    <div onClick={handleClick}>
-      {isOn ? <SwitchOn/> : <SwitchOff/>}
-    </div>
+      <div>
+        <div className={styles.port}/>
+        <div onMouseUp={handleMouseUp}
+             onMouseDown={() => setIsMouseDown(true)}
+             onMouseMove={() => handleMouseMove(true)}
+             className={styles.component}>
+          {isOn ? <SwitchOn/> : <SwitchOff/>}
+        </div>
+        <div className={styles.port}/>
+      </div>
   )
 }
